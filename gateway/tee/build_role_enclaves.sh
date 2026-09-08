49d2e3dc1f4800aa598d32cff85c683d3f884984
Pranav-create <pranav@leadpoet.com>
2026-09-08 12:35:45 -0700

    Remove temporary Nitro build environment

--- gateway/tee/build_role_enclaves.sh
+++ gateway/tee/build_role_enclaves.sh
@@ -34,11 +34,6 @@ publish_built_eif_for_verification() {
   }
 }
 
-build_enclave_with_nitro_cli() {
-  sudo --preserve-env=NITRO_CLI_ARTIFACTS,NITRO_CLI_BLOBS \
-    nitro-cli build-enclave "$@"
-}
-
 . "$REPO_ROOT/validator_tee/scripts/docker_operation_lock_v2.sh"
 leadpoet_acquire_docker_operation_lock_v2
 
@@ -141,7 +136,7 @@ if [ "$RESTORED_EXACT_RELEASE" != "1" ]; then
       --normalized-image "$image"
     sudo docker image inspect -f '{{.Id}}' "$image" \
       > "$BUILD_EIF_ROOT/enclave-image-${role}.txt"
-    build_enclave_with_nitro_cli \
+    sudo nitro-cli build-enclave \
       --docker-uri "$image" \
       --output-file "$output" \
       | tee "$measurements"

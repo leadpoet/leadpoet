6bd27ff9c52bbdb8f4f46dcfd8d6039a61133e2c
Pranav-create <pranav@leadpoet.com>
2026-09-11 10:38:51 -0700

    Wire contact activation into testnet shadow launcher

--- scripts/validate_miner_testnet.py
+++ scripts/validate_miner_testnet.py
@@ -655,6 +655,7 @@ def _serve(args: argparse.Namespace) -> int:
             daily_cutoff_hour_utc=None,
             rewards_enabled=False,
             integrity_from=os.environ.get("LAB_ARENA_INTEGRITY_FROM") or None,
+            contacts_from=os.environ.get("LAB_ARENA_CONTACTS_FROM") or None,
             benchmark_disclosure_from=os.environ.get("LAB_ARENA_BENCHMARK_DISCLOSURE_FROM") or None,
         )
         service = ArenaService(
